5c374ec8e8bde7db644ee77f86f6a0be62085171
Fabio Fernandez <ffernan9@jhu.edu>
2024-07-06 09:51:41 -0400

    init repo

---
+++ cutSprites.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd $WORKDIR/assets/minecraft/optifine/ctm
+
+for path in $DOWNLOADS/log-spritesheet-variants/*_variants.png; do
+  
+  file_name="${path#$DOWNLOADS/log-spritesheet-variants/}"
+  wood_type="${file_name%_variants.png}"
+  
+  cd $wood_type
+
+  rm *.png
+  convert $path -crop 16x16 +repage -scene 1 %d.png
+  cp $DOWNLOADS/log-sprite-defaults/$wood_type.png 0.png
+  chmod -x *.png
+
+  cd ..
+
+done
---
+++ zip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd $WORKDIR
+
+zip -9rq Knotted_Wood_1.20.4.zip assets pack.*
